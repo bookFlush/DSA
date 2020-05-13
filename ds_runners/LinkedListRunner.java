25814b1628c497349006856486977e05fb60e51c
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2020-05-14 00:15:13 +0530

    first commit

---
+++ ds_runners/LinkedListRunner.java
@@ -0,0 +1,11 @@
+package ds_runners;
+
+import ds_linkedList.SinglyLinkedList;
+
+public class LinkedListRunner extends SinglyLinkedList{
+	public static void main(String[] args) {
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.getSize();
+		
+	}
+}
